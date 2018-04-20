cd794cb8b78ebe614a3a44e3f62801eba5d2a83a
Alpesh <alpeshpb@gmail.com>
2018-04-20 12:02:31 -0400

    v1.05

--- database.py
+++ database.py
@@ -1,33 +1,38 @@
 import psycopg2
 import os
+import traceback
 #from database_private import DatabasePrivateSettings
 
 class DatabaseGateway():
 
-    def __init__(self):
-#        if (env == "LOCAL"):
-#            self.DATABASE_URL = DatabasePrivateSettings.DATABASE_URL
-#        else:
+    def __init__(self, env):
+        if (env == "LOCAL"):
+            #self.DATABASE_URL = DatabasePrivateSettings.DATABASE_URL
+        else:
             self.DATABASE_URL = os.environ['DATABASE_URL']
 
     def connect(self):
         self.connection = psycopg2.connect(self.DATABASE_URL, sslmode='require')
         self.cursor = self.connection.cursor()
+        self.isClosed = False
 
     def commit(self):
         self.connection.commit()
 
     def close(self):
+        self.cursor.close()
         self.connection.close()
+        self.isClosed = True
 
     def insertHistroicalPrice(self, row, symbolandExchange):
         try:
-            if (self.connection != 0):
+            if (self.isClosed is True):
                 self.connect()
             self.cursor.execute("INSERT INTO historical_prices VALUES (%s, %s, %s, %s, %s, %s, %s)", (row[0], symbolandExchange, row[1], row[2], row[3], row[4], row[5]))
             self.commit()
             return False
-        except psycopg2.Error as e:
-            print(e.pgcode + "\n" + e.pgerror)
-            self.connection.close()
+        except Exception as e:
+            #print(e.pgcode + "\n" + e.pgerror)
+            print(traceback.format_exc())
+            self.close()
             return True
--- stock-scan.py
+++ stock-scan.py
@@ -16,7 +16,7 @@ for stock in helper.stockList:
 
 
     #connect to database
-    db = DatabaseGateway()
+    db = DatabaseGateway("PROD")
     db.connect()
 
     j = 0
@@ -27,6 +27,6 @@ for stock in helper.stockList:
                 row[5] = int(row[5])
             except ValueError:
                 row[5] = 0
-            isError = db.insertHistroicalPrice(row, stock)
+            db.insertHistroicalPrice(row, stock)
         j = j + 1
     db.close()
